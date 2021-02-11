ed76dd5846a2a3cc122ad1aaf2387daee20c9b91
Felix Soubelet <felix.soubelet@protonmail.com>
2021-02-11 14:31:05 +0100

    fixed version, needed MAKETHIN to succeed matching somehow

--- Local_Coupling/2_colin_disp.py
+++ Local_Coupling/2_colin_disp.py
@@ -6,13 +6,14 @@ from typing import List, Union
 from cpymad.madx import Madx
 from loguru import logger
 from pydantic import BaseModel
-from pyhdtoolkit.cpymadtools import latwiss, matching, special
+from pyhdtoolkit.cpymadtools import latwiss, matching, orbit, special
 from pyhdtoolkit.utils.defaults import LOGURU_FORMAT
 
 # ----- Setup ----- #
 
 PATHS = {
     "optics2018": Path("/afs/cern.ch/eng/lhc/optics/runII/2018"),
+    "local": Path("/Users/felixsoubelet/cernbox/OMC/MADX_scripts/Local_Coupling"),
     "htc_outputdir": Path("Outputdata"),
 }
 
@@ -31,7 +32,7 @@ class Results(BaseModel):
     def to_json(self, filepath: Union[str, Path]) -> None:
         logger.debug(f"Exporting results structure to '{Path(filepath).absolute()}'")
         with Path(filepath).open("w") as results_file:
-            json.dump(results.dict(), results_file)
+            json.dump(self.dict(), results_file)
 
 
 def cycle_from_ip3(madx: Madx, sequence: str = "lhcb1") -> None:
@@ -50,7 +51,7 @@ def fullpath(filepath: Path) -> str:
 # ----- Simulation ----- #
 
 
-def make_simulation(colin_knob: float = 0.0, rigidity_knob: float = 0.0) -> None:
+def make_simulation(colin_knob: float = 0.0, rigidity_knob: float = 0.0) -> Results:
     """
     Get a complete LHC setup, implement colinearity knob and rigidity waist shift knob, get the Cminus.
     Both parameters and the results are stored in a structure and exported to JSON.
@@ -59,7 +60,7 @@ def make_simulation(colin_knob: float = 0.0, rigidity_knob: float = 0.0) -> None
         colin_knob (float): unit setting of the colinearity knob.
         rigidity_knob (float): unit setting of the rigidity waist shift knob.
     """
-    madx = Madx(stdout=False)
+    madx = Madx(command_log=fullpath(PATHS["htc_outputdir"] / "cpymad_commands.log"))
     madx.option(echo=False, warn=False)
     logger.info(
         f"Running with a colinearity knob of {colin_knob} and a rigidity waist shift knob of {rigidity_knob}"
@@ -70,36 +71,43 @@ def make_simulation(colin_knob: float = 0.0, rigidity_knob: float = 0.0) -> None
     chrom_x, chrom_y = 2.0, 2.0
 
     logger.info("Calling optics")
-    madx.call(fullpath(PATHS["optics2018"] / "lhc_as-built.seq"))
-    madx.call(fullpath(PATHS["optics2018"] / "PROTON" / "opticsfile.22"))
+    madx.call(fullpath(PATHS["optics2018"] / "lhc_as-built.seq"))  # afs
+    madx.call(fullpath(PATHS["optics2018"] / "PROTON" / "opticsfile.22"))  # afs
+    # madx.call(fullpath(PATHS["local"] / "sequences" / "lhc_as-built.seq"))  # local testing
+    # madx.call(fullpath(PATHS["local"] / "optics" / "opticsfile.22"))  # local testing
+    madx.command.beam()
+    special.make_lhc_thin(madx, sequence="lhcb1", slicefactor=8)
     cycle_from_ip3(madx, sequence="lhcb1")
 
     logger.info("Setting up orbit, creating beams and matching working point")
     orbit_scheme = orbit.setup_lhc_orbit(madx, scheme="flat")
-    special.make_lhc_beams(madx, energy=6500, emittance=3.75e-6, npart=1.0e10)
+    special.make_lhc_beams(madx, energy=6500, emittance=3.75e-6)
     madx.use(sequence="lhcb1")
     matching.match_tunes_and_chromaticities(
-        madx, "lhc", "lhcb1", tune_x, tune_y, chrom_x, chrom_y, telescopic_squeeze=True
+        madx, "lhc", "lhcb1", tune_x, tune_y, chrom_x, chrom_y, calls=200, telescopic_squeeze=True
     )
 
     logger.info("Setting up colinearity and rigidity waist shift knobs for IR1")
     special.apply_lhc_colinearity_knob(madx, colinearity_knob_value=colin_knob, ir=1)
     special.apply_lhc_rigidity_waist_shift_knob(madx, rigidty_waist_shift_value=rigidity_knob, ir=1)
     matching.match_tunes_and_chromaticities(
-        madx, "lhc", "lhcb1", tune_x, tune_y, chrom_x, chrom_y, telescopic_squeeze=True
+        madx, "lhc", "lhcb1", tune_x, tune_y, chrom_x, chrom_y, calls=200, telescopic_squeeze=True
     )
 
     logger.info("Executing Closest Tune Approach\n")
-    madx.twiss(chrom=True, ripken=True)
-    dqmin = matching.get_closest_tune_approach(madx, "lhc", "lhcb1", telescopic_squeeze=True)
-    madx.twiss(chrom=True, ripken=True)
+    dqmin = matching.get_closest_tune_approach(madx, "lhc", "lhcb1")
+    madx.exit()
 
-    results = Results(rigidity_knob=rigidity_knob, colin_knob=colin_knob, dqmin=dqmin)
-    results.to_json(PATHS["htc_outputdir"] / "result_params.json")
+    return Results(rigidity_knob=rigidity_knob, colin_knob=colin_knob, dqmin=dqmin)
 
 
 if __name__ == "__main__":
-    make_simulation(
-        colin_knob=%(COLIN_KNOB)s,
-        rigidity_knob=%(RIGIDITY_WAIST_SHIFT_KNOB)s,
+    simulation_results = make_simulation(  # afs run
+    colin_knob=%(COLIN_KNOB)s,
+    rigidity_knob=%(RIGIDITY_WAIST_SHIFT_KNOB)s,
     )
+    # simulation_results = make_simulation(  # local testing
+    #     colin_knob=-3.0,
+    #     rigidity_knob=1,
+    # )
+    simulation_results.to_json(PATHS["htc_outputdir"] / "result_params.json")
