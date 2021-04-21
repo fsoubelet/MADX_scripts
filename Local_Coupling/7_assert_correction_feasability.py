2fb8cdc8f8e756b5c74b52748fe49c33802085a9
Felix Soubelet <felix.soubelet@protonmail.com>
2021-04-21 16:11:04 +0200

    cleared up script, needs pyhd>=0.9.0, takes QUADRUPOLES are for htc.

--- Local_Coupling/7_assert_correction_feasability.py
+++ Local_Coupling/7_assert_correction_feasability.py
@@ -10,7 +10,7 @@ import pyhdtoolkit
 from cpymad.madx import Madx
 from loguru import logger
 from pydantic import BaseModel
-from pyhdtoolkit.cpymadtools import matching, orbit, special
+from pyhdtoolkit.cpymadtools import errors, matching, orbit, special
 from pyhdtoolkit.utils import defaults
 
 # ----- Setup ----- #
@@ -22,7 +22,11 @@ PATHS = {
 }
 
 defaults.config_logger(level="DEBUG")
-logger.add(PATHS["htc_outputdir"] / "full_pylog.log", format=defaults.LOGURU_FORMAT, level="TRACE")
+logger.add(
+    PATHS["htc_outputdir"] / "full_pylog.log",
+    format=defaults.LOGURU_FORMAT,
+    level="TRACE",
+)
 
 # ----- Utilities ----- #
 
@@ -42,6 +46,10 @@ class Results(BaseModel):
     beta12_std: float
     beta21_value: float
     beta21_std: float
+    kqsx3_l1_value: float
+    kqsx3_l1_std: float
+    kqsx3_r1_value: float
+    kqsx3_r1_std: float
 
     def to_json(self, filepath: Union[str, Path]) -> None:
         logger.debug(f"Exporting results structure to '{Path(filepath).absolute()}'")
@@ -63,58 +71,7 @@ def fullpath(filepath: Path) -> str:
     return str(filepath.absolute())
 
 
-def misalign_lhc_q1_to_q6(
-    madx: Madx, ip: int, beam: int, sides: Sequence[str] = ("r", "l"), table: str = "errors", **kwargs
-) -> None:
-    """
-    Apply misalignment errors to triplet quadrupoles on a given side of a given IP. In case of a sliced
-    lattice, this will misalign all slices of the magnet together.
-
-    Args:
-        madx (cpymad.madx.Madx): an instanciated cpymad Madx object.
-        ip (int): the interaction point around which to apply errors.
-        beam (int): beam number to apply the errors to. Unlike triplet quadrupoles which are single
-            aperture, Q4 to Q6 are not.
-        sides (Sequence[str]): sides of the IP to apply error on the triplets, either L or R or both.
-            Defaults to both.
-        table (str): the name of the internal table that will save the assigned errors. Defaults to
-            'triplet_errors'.
-
-    Keyword Args:
-        Any keyword argument to give to the EALIGN command, including the error to apply (DX, DY,
-        DPSI etc) as a string, like it would be given directly into MAD-X.
-
-    Examples:
-        dist_center = 1e-3  # for rad tilt
-        misalign_lhc_q4_q6(madx, ip=1, sides="RL", dx="1E-5 * TGAUSS(2.5)")
-        misalign_lhc_q4_q6(madx, ip=5, sides="RL", dpsi=f"{dist_center} * TGAUSS(2.5)")
-    """
-    if ip not in (1, 2, 5, 8) or any(side.upper() not in ("R", "L") for side in sides):
-        logger.error("Either the IP number of the side provided are invalid, not applying any error.")
-        raise ValueError("Invalid 'ip' or 'sides' argument")
-
-    logger.info(f"Applying alignment errors to individual quadrupoles 4 to 6, with arguments {kwargs}")
-    madx.command.select(flag="error", clear=True)
-    for side in sides:
-        logger.debug(f"Applying errors on {'right' if side.upper() == 'R' else 'left'} side of IP{ip:d}")
-        madx.command.select(flag="error", pattern=f"^MQXA.1{side.upper()}{ip:d}")  # Q1 LHC
-        madx.command.select(flag="error", pattern=f"^MQXFA.[AB]1{side.upper()}{ip:d}")  # Q1A & Q1B HL-LHC
-        madx.command.select(flag="error", pattern=f"^MQXB.[AB]2{side.upper()}{ip:d}")  # Q2A & Q2B LHC
-        madx.command.select(flag="error", pattern=f"^MQFXB.[AB]2{side.upper()}{ip:d}")  # Q2A & Q2B HL-LHC
-        madx.command.select(flag="error", pattern=f"^MQXA.3{side.upper()}{ip:d}")  # Q3 LHC
-        madx.command.select(flag="error", pattern=f"^MQXFA.[AB]3{side.upper()}{ip:d}")  # Q3A & Q3B HL-LHC
-        madx.command.select(flag="error", pattern=f"^MQY.4{side.upper()}{ip:d}.B{beam:d}")  # Q4 (HL)LHC
-        madx.command.select(flag="error", pattern=f"^MQML.5{side.upper()}{ip:d}.B{beam:d}")  # Q5 (HL)LHC
-        madx.command.select(flag="error", pattern=f"^MQML.6{side.upper()}{ip:d}.B{beam:d}")  # Q6 (HL)LHC
-    madx.command.ealign(**kwargs)
-
-    logger.debug(f"Saving assigned errors in internal table '{table if table else 'etable'}'")
-    madx.command.etable(table=table)
-
-    logger.trace("Clearing up error flag")
-    madx.command.select(flag="error", clear=True)
-
-
+# TODO: include this in pyhdtoolkit?
 def match_no_coupling_at_ip_through_rterms(madx: Madx, sequence: str, ip: int) -> None:
     """
     Matching commands to get R-matrix terms to be 0 at given IP, using skew quad correctors independently.
@@ -134,13 +91,18 @@ def match_no_coupling_at_ip_through_rterms(madx: Madx, sequence: str, ip: int) -
 # ----- Simulation ----- #
 
 
-def make_simulation(tilt_mean: float = 0.0, seeds: int = 50) -> Results:
+def make_simulation(
+    tilt_mean: float = 0.0, quadrupoles: List[int] = [1, 2, 3, 4, 5, 6], seeds: int = 50
+) -> Results:
     """
     Get a complete LHC setup, implement colinearity knob and rigidity waist shift knob, get the Cminus.
     Both parameters and the results are stored in a structure and exported to JSON.
 
     Args:
-        tilt_mean (float): mean value of the dpsi tilt distribution when applying to Q1-6 quadrupoles.
+        tilt_mean (float): mean value of the dpsi tilt distribution when applying to quadrupoles. To be
+            provided throught htcondor submitter if running in CERN batch.
+        quadrupoles (List[int]) the list of quadrupoles to apply errors to. Defaults to Q1 to A6, to be
+            provided throught htcondor submitter if running in CERN batch.
         seeds (int): the number of runs with different seeds to do for each tilt value.
     """
     tilt_errors: List[pd.DataFrame] = []
@@ -156,29 +118,35 @@ def make_simulation(tilt_mean: float = 0.0, seeds: int = 50) -> Results:
             logger.debug("Calling optics")
             # madx.call(fullpath(PATHS["optics2018"] / "lhc_as-built.seq"))  # afs
             # madx.call(fullpath(PATHS["optics2018"] / "PROTON" / "opticsfile.22"))  # afs
-            madx.call(fullpath(PATHS["local"] / "sequences" / "lhc_as-built.seq"))  # local testing
-            madx.call(fullpath(PATHS["local"] / "optics" / "opticsfile.22"))  # local testing
+            # madx.call(fullpath(PATHS["local"] / "sequences" / "lhc_as-built.seq"))  # local testing
+            # madx.call(fullpath(PATHS["local"] / "optics" / "opticsfile.22"))  # local testing
 
             special.re_cycle_sequence(madx, sequence="lhcb1", start="IP3")
             orbit_scheme = orbit.setup_lhc_orbit(madx, scheme="flat")
             special.make_lhc_beams(madx, energy=6500, emittance=3.75e-6)
             madx.use(sequence="lhcb1")
-            matching.match_tunes_and_chromaticities(
-                madx, "lhc", "lhcb1", 62.31, 60.32, 2.0, 2.0, calls=200, telescopic_squeeze=True
-            )
+            matching.match_tunes_and_chromaticities(madx, "lhc", "lhcb1", 62.31, 60.32, 2.0, 2.0, calls=200)
 
             logger.info("Applying misalignments to IR quads 1 to 6")
-            misalign_lhc_q1_to_q6(
-                madx, ip=1, beam=1, sides="RL", dpsi=f"{tilt_mean} + {abs(tilt_mean) * 0.15} * TGAUSS(2.5)"
+            errors.misalign_lhc_ir_quadrupoles(  # requires pyhdtoolkit >= 0.9.0
+                madx,
+                ip=1,
+                beam=1,
+                quadrupoles=quadrupoles,
+                sides="RL",
+                dpsi=f"{tilt_mean} + {abs(tilt_mean) * 0.15} * TGAUSS(2.5)",
             )
-            tilt_errors.append(
-                madx.table.errors.dframe().copy().set_index("name", drop=True).loc[:, ["dpsi"]]
-            )  # just get the dpsi column, save memory
+            tilt_errors.append(  # just get the dpsi column, save memory
+                madx.table.ir_quads_errors.dframe().copy().set_index("name", drop=True).loc[:, ["dpsi"]]
+            )
 
             match_no_coupling_at_ip_through_rterms(madx, sequence="lhcb1", ip=1)
             madx.twiss(ripken=True)
-            corrected_twisses.append(madx.table.twiss.dframe().copy().set_index("name", drop=True))
+            twiss_df = madx.table.twiss.dframe().copy().set_index("name", drop=True)
+            twiss_df["k1s"] = twiss_df.k1sl / twiss_df.l
+            corrected_twisses.append(twiss_df)
 
+    logger.info("Aggregating results from all seeds")
     all_errors = pd.concat(tilt_errors)  # concatenating all errors for this tilt's runs
     all_results = pd.concat(corrected_twisses)  # concatenating all resulting twisses for this tilt's runs
 
@@ -197,6 +165,10 @@ def make_simulation(tilt_mean: float = 0.0, seeds: int = 50) -> Results:
         beta12_std=all_results.beta12.std(),
         beta21_value=all_results.beta21.mean(),
         beta21_std=all_results.beta21.std(),
+        kqsx3_l1_value=all_results.loc[all_results.index == "mqsx.3l1:1"].k1s.mean(),
+        kqsx3_l1_std=all_results.loc[all_results.index == "mqsx.3l1:1"].k1s.std(),
+        kqsx3_r1_value=all_results.loc[all_results.index == "mqsx.3r1:1"].k1s.mean(),
+        kqsx3_r1_std=all_results.loc[all_results.index == "mqsx.3r1:1"].k1s.std(),
     )
 
 
@@ -208,12 +180,14 @@ if __name__ == "__main__":
         logger.critical(
             f"Using: pyhdtoolkit {pyhdtoolkit.__version__} | cpymad {cpymad.__version__}  | {mad.version}"
         )
-    simulation_results = make_simulation(  # afs run
-        tilt_mean=%(DPSI_MEAN)s,
-        seeds=%(SEEDS)s,
-    )
+    # simulation_results = make_simulation(  # afs run
+    #     tilt_mean=%(DPSI_MEAN)s,
+    #     quadrupoles=%(QUADRUPOLES)s,
+    #     seeds=%(SEEDS)s,
+    # )
     # simulation_results = make_simulation(  # local testing
     #     tilt_mean=5e-4,
-    #     seeds=2,
+    #     quadrupoles=[1, 2, 3, 4, 5, 6],
+    #     seeds=5,
     # )
     simulation_results.to_json(PATHS["htc_outputdir"] / "results.json")
